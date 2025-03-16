f3d2ceeb04680e20056c5a58e6d58f2541be9e8d
Nanou2075 <nanouroukou2075@gmail.com>
2025-03-16 13:37:37 +0000

    refactoring

--- src/main/java/com/elearning/elearning/account/AccountResource.java
+++ src/main/java/com/elearning/elearning/account/AccountResource.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("account")
+@RequestMapping("account/")
 public interface AccountResource {
     @GetMapping("get-account/{accountId}")
     Account getAccount(@PathVariable String accountId);
     @GetMapping("get/{accountId}")
     AccountResponse getAccountResponse(@PathVariable String accountId);
 
-    @GetMapping("get/permission")
+    @GetMapping("permission")
     Response getPermission();
 
     @GetMapping("statistical")
